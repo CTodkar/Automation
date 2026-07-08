6537eb3a1621fa9e282b06d02c42700401e96dac
CTODKAR <71268215+CTodkar@users.noreply.github.com>
2026-07-08 22:14:45 +0530

    Enhance HealthMonitor script with debug and CPU checks
    Added debug mode options and additional CPU checks.

--- HealthMonitor.sh
+++ HealthMonitor.sh
@@ -3,7 +3,9 @@
 #Below is the script to monitor the Linux Server
 
 #Enable the debug mode
-set -X
+set -x  #To run script in debug mode
+set -e  #To exit script when there is an error exit 
+set -o pipefail #When there is an pipefail
 
 echo "To check free space"
 df -h
@@ -13,5 +15,8 @@ ps -ef
 
 echo "To check total available cpu"
 nproc
+
+echo "To check total available cpu"
+ps -ef | grep aws | awk -F" " '{print $2}'
 
 exit 0
