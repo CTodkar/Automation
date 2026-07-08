914855d329b20666bdf3d055ac84ce4b9526a97a
CTODKAR <71268215+CTodkar@users.noreply.github.com>
2026-07-08 15:25:25 +0530

    linux node check script
    Commands to check processes and CPU availability

---
+++ HealthMonitor.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#Below is the script to monitor the Linux Server
+
+#Enable the debug mode
+set -X
+
+echo "To check free space"
+df -h
+
+echo "To list all the processes running"
+ps -a 
+
+echo "To check total available cpu"
+nproc
+
+exit 0
